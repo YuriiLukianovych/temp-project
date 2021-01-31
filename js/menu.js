4fc3533894eb5df3475e9c24997e2d266a8c4309
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-01-31 21:56:09 +0200

    edited buttons open/close burger-menu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
+  const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const bodyh2 = document.querySelector("[data-body]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-      // bodyh2.classList.toggle('is-blocked');
-    });
-  })();
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
